08984982708b65e7e269207acca4cb2bb0ad8adc
Sami Hamed <sami.k.hamed@googlemail.com>
2019-10-18 20:20:48 +0200

    :children_crossing: Move higher quality content to top of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import Body from './components/Body'
-import Projects from './components/Projects'
-import Tiles from './components/Tiles'
-import Rainbow from './components/Rainbow'
-import Blog from './components/Blog'
-import Footer from './components/Footer';
+import Body from "./components/Body";
+import Projects from "./components/Projects";
+import Tiles from "./components/Tiles";
+import Rainbow from "./components/Rainbow";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
 
-import './styles/App.css'
-import './styles/Typography.css'
-
+import "./styles/App.css";
+import "./styles/Typography.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Body/>
-      <Projects/>
-      <Tiles/>
-      <Rainbow/>
-      <Blog/>
-      <Footer/>
+      <Body />
+      <Projects />
+      <Blog />
+      <Rainbow />
+      <Tiles />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
